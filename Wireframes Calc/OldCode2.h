6d5cb8e8aaddc3730cb7c7fdaad43de77943d2f5
Aaron Pfitzenmaier <aaron@pfitzpr.com>
2016-06-30 13:58:11 -0500

    Add OldCode2 file

---
+++ Wireframes Calc/OldCode2.h
@@ -0,0 +1,14 @@
+//
+//  OldCode2.h
+//  Wireframes Calc
+//
+//  Created by Aaron Pfitzenmaier on 6/30/16.
+//  Copyright (c) 2016 Aaron Pfitzenmaier. All rights reserved.
+//
+
+#ifndef __Wireframes_Calc__OldCode2__
+#define __Wireframes_Calc__OldCode2__
+
+void oldCode2(); //5/28/16 to 6/22/16
+
+#endif /* defined(__Wireframes_Calc__OldCode2__) */
